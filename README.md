7d63dd0098cc2f79a528ab6c5f4518eecd7ebc8c
Kadir Yamuk <kadir@enpama.com>
2021-08-03 12:07:41 +0300

    Add support for Angular and Electron

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # little-bang
-Create project templates, folder structures, actions, initial files and settings for your coding projects.
+Create project templates, folder structures, actions, initial files and settings for your projects.
+
+The purpose of this application is to shorten and automate the initial process of development. 
+
+## Installation
+Installation instructions will be available when the project reaches a runnable state.
+
+The program uses node so it will be similar to any other node application.
+
+## Features
+- Templates
+- Run external commands via hooks
+- Initial files and folder with default content
+- Variables
+- Integration to various services
---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: '#ffffff',
+        icon: __dirname + '/dist/assets/logo-96x96.png',
+    });
+
+    win.loadURL('file://' + __dirname + '/dist/index.html');
+
+    win.webContents.openDevTools()
+    win.on('closed', function () {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (win === null) {
+        createWindow();
+    }
+});
